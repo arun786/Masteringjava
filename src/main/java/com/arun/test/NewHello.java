5179cf0dfd7bb457a01c9c039ab7c65753fd3bb3
arun789 <adwiti1975@gmail.com>
2017-07-09 00:47:37 -0700

    understanding whole numbers

---
+++ src/main/java/com/arun/test/NewHello.java
@@ -0,0 +1,14 @@
+package com.arun.test;
+
+/**
+ * Created by Adwiti on 7/6/2017.
+ */
+public class NewHello {
+    public static void main(String[] args) {
+        System.out.println("Hello Arun!");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        int mySecondNumber = 10;
+        System.out.println(myFirstNumber + mySecondNumber);
+    }
+}
