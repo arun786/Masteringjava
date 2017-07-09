265a5659af767cab95e7959fea04a977d1bfa13d
arun789 <adwiti1975@gmail.com>
2017-07-09 00:39:35 -0700

    understanding whole numbers

---
+++ src/main/java/com/arun/datatypes/LongDataType.java
@@ -0,0 +1,15 @@
+package com.arun.datatypes;
+
+/**
+ * Created by Adwiti on 7/9/2017.
+ */
+public class LongDataType {
+
+    public static void main(String[] args) {
+        /*Long has a width of 64 bits*/
+        long myLongValue = 100L;
+        System.out.println(myLongValue);
+        long myNewLongValue = myLongValue/2;
+        System.out.println(myNewLongValue);
+    }
+}
