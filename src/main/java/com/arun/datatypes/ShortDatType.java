68e2bd868c549d228990b26fbf03f06e3c5701f5
arun789 <adwiti1975@gmail.com>
2017-07-09 00:45:03 -0700

    understanding whole numbers

---
+++ src/main/java/com/arun/datatypes/ShortDatType.java
@@ -0,0 +1,16 @@
+package com.arun.datatypes;
+
+/**
+ * Created by Adwiti on 7/8/2017.
+ */
+public class ShortDatType {
+    public static void main(String[] args) {
+
+        /*short has a width of 16 bits*/
+        short myShortMinValue = -32767;
+        short myShortMaxValue = 32767;
+
+        System.out.println(myShortMaxValue);
+        System.out.println(myShortMinValue);
+    }
+}
