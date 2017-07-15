ed041763d3a19016b8cb7f7edd64f310436b20eb
arun789 <adwiti1975@gmail.com>
2017-07-15 12:51:21 -0700

    char and boolean

---
+++ src/main/java/com/arun/datatypes/charBoolean.java
@@ -0,0 +1,14 @@
+package com.arun.datatypes;
+
+/**
+ * Created by Adwiti on 7/15/2017.
+ */
+public class charBoolean {
+    public static void main(String[] args) {
+        char registeredSymbol = '\u00AE';
+        System.out.println("unicode output is " + registeredSymbol);
+
+        boolean isMale = true;
+        System.out.println("Arun is a male " + isMale);
+    }
+}
