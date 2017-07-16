6c7e6ce5d50299126f7de96fcdc38d3074877a28
arun789 <adwiti1975@gmail.com>
2017-07-16 15:19:19 -0700

    operators precedence

---
+++ src/main/java/com/arun/datatypes/Variables.java
@@ -0,0 +1,13 @@
+package com.arun.datatypes;
+
+/**
+ * Created by Adwiti on 7/16/2017.
+ */
+public class Variables {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 3;
+        int d = ( a + b /3 - 4 * 12);
+        System.out.println(d); // -35
+    }
+}
