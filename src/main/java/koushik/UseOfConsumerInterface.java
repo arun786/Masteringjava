ad4ca7d935a02e953c461125c1bab7724eba261b
arun789 <adwiti1975@gmail.com>
2017-07-24 23:54:49 -0700

    use of consumer and predicate interface

--- src/main/java/koushik/UseOfConsumerInterface.java
+++ src/main/java/koushik/UseOfConsumerInterface.java
@@ -3,6 +3,7 @@ package koushik;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Created by Adwiti on 7/24/2017.
@@ -13,11 +14,20 @@ public class UseOfConsumerInterface {
         List<Container> containers = Arrays.asList(new Container("BlackContainer", 100), new Container("WhiteContainer", 200), new Container("BlueContainer", 300));
         findAllContainer(containers, e -> System.out.println(e.getNameOfcontainer()));
         findAllContainer(containers, e -> System.out.println(e.getPrice()));
+        findAllContainerUsingFilter(containers, e -> e.getPrice() > 100, e -> System.out.println(e.getNameOfcontainer() + " : " + e.getPrice()));
     }
 
     static void findAllContainer(List<Container> containers, Consumer<Container> printer) {
         containers.forEach(container -> {
             printer.accept(container);
+        });
+    }
+
+    static void findAllContainerUsingFilter(List<Container> containers, Predicate<Container> predicate, Consumer<Container> printer) {
+        containers.forEach(container -> {
+            if (predicate.test(container)) {
+                printer.accept(container);
+            }
         });
     }
 }
